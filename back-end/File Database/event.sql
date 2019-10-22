49b760e10d3581c0e84d2db57ee122708cf256d5
nguyenhuyy99 <nguyenhuyy99@gmail.com>
2019-10-22 18:26:31 +0700

    show trang chu

--- back-end/File Database/event.sql
+++ back-end/File Database/event.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2019 at 04:40 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- Generation Time: Oct 22, 2019 at 03:20 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `ten_su_kien` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_loai` int(10) NOT NULL,
+  `banner` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ngay_dien_ra` datetime DEFAULT NULL,
+  `anh_su_kien` int(11) DEFAULT NULL,
+  `gia_ve` double(8,2) DEFAULT NULL,
+  `dia_chi` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ngay_ban` datetime DEFAULT NULL,
+  `mo_ta` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `so_luong_ve` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `ten_su_kien`, `id_loai`, `banner`, `ngay_dien_ra`, `anh_su_kien`, `gia_ve`, `dia_chi`, `ngay_ban`, `mo_ta`, `so_luong_ve`) VALUES
+(1, 'Sự kiện test', 1, '1.jpg', '2019-10-02 00:00:00', 0, 1.00, 'Hà Nội', '0000-00-00 00:00:00', NULL, 0),
+(2, 'Sự kiện giải trí', 1, '2.jpg', '2019-10-03 00:00:00', NULL, 1.00, 'TP HCM', NULL, NULL, NULL),
+(3, 'Sự kiện 1', 1, '3.jpg', '2019-10-17 00:00:00', NULL, 100000.00, 'TP HCM', NULL, NULL, NULL),
+(4, 'Sự kiện kiến thức ', 2, '5.jpg', '2019-10-10 00:00:00', NULL, NULL, 'Hà Nội', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `failed_jobs`
 --
 
@@ -34,7 +64,20 @@ CREATE TABLE `failed_jobs` (
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images_event`
+--
+
+CREATE TABLE `images_event` (
+  `id` int(10) NOT NULL,
+  `duong_dan` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -57,7 +100,38 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (4, '2014_10_12_000000_create_users_table', 1),
 (5, '2014_10_12_100000_create_password_resets_table', 1),
 (6, '2019_08_19_000000_create_failed_jobs_table', 1),
-(7, '2019_10_19_135957_slider', 2);
+(7, '2019_10_19_135957_slider', 2),
+(8, '2019_10_20_235835_user', 3),
+(9, '2019_10_21_213909_events', 3),
+(10, '2019_10_21_214921_type_events', 4),
+(11, '2019_10_21_215139_images_event', 5),
+(12, '2019_10_21_215741_news', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `tieu_de` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `noi_dung` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tom_tat` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hinh_anh` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hinh_anh_hien_thi` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `video` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ngay_dang` datetime DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `tieu_de`, `noi_dung`, `tom_tat`, `hinh_anh`, `hinh_anh_hien_thi`, `video`, `ngay_dang`, `created_at`, `updated_at`) VALUES
+(1, 'Tin tức', NULL, 'Demo Show tin tức', NULL, '4.png', NULL, '2019-10-24 00:00:00', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -99,6 +173,46 @@ INSERT INTO `slider` (`id`, `ten_su_kien`, `banner`, `loai_su_kien`, `thoi_gian`
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `type_events`
+--
+
+CREATE TABLE `type_events` (
+  `id` int(10) NOT NULL,
+  `ten_loai` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `type_events`
+--
+
+INSERT INTO `type_events` (`id`, `ten_loai`, `created_at`, `updated_at`) VALUES
+(1, 'Giải trí', NULL, NULL),
+(2, 'Kiến thức', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dien_thoai` int(11) NOT NULL,
+  `dia_chi` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ngay_sinh` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gioi_tinh` tinyint(1) NOT NULL,
+  `hinh` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `vip` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -131,15 +245,34 @@ INSERT INTO `users` (`id`, `name`, `dien_thoai`, `hinh`, `gioi_tinh`, `ten`, `id
 --
 
 --
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_loai` (`id_loai`);
+
+--
 -- Indexes for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `images_event`
+--
+ALTER TABLE `images_event`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -155,6 +288,18 @@ ALTER TABLE `slider`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `type_events`
+--
+ALTER TABLE `type_events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -163,6 +308,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -174,7 +325,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `slider`
@@ -183,10 +340,32 @@ ALTER TABLE `slider`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `type_events`
+--
+ALTER TABLE `type_events`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`id_loai`) REFERENCES `type_events` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- back-end/event/app/Http/Controllers/PagesController.php
+++ back-end/event/app/Http/Controllers/PagesController.php
@@ -4,48 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-<<<<<<< HEAD
+
 use App\users;
-=======
+
 use App\Slide;
->>>>>>> d12c48ef7219dc9621ba0e165d60dafed943455d
+
+use App\Events;
+
+use App\News;
 class PagesController extends Controller
 {
     public function getIndex(){
         $slide = Slide::all();
-        return view('pages.body',['slide'=>$slide ]);
+        $giaitri = Events::where('id_loai',1)->get();
+        $kienthuc = Events::where('id_loai',2)->get();
+        $new_01= News::get();
+        return view('pages.body',compact('slide','giaitri','kienthuc','new_01'));
     }
     public function getSreach(){
         return view('pages.sreach');
     }
     public function getLogin(){
-<<<<<<< HEAD
         return view('pages.login');
     }
-=======
-
-        return view('pages.login');
-    }
-    public function postLogin(Request $request){
-        $this->validate($request,
-        [
-            'email'=>'required',
-            'password'=>'required|min:3|max:50',
-        ]
-        ,
-        [
-            'email.required'=>'Bạn chưa nhập email',
-            'password.required'=>'Bạn chưa nhập mật khẩu',
-            'password.min'=>'Mật khẩu phải lơn hơn 3 kí tự',
-            'password.max'=>'Mật khẩu không quá 50 kí tự',
-        ]);
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
-            return redirect('pages/body');
-        } else{
-            return redirect('pages/login')->with('thongbao', 'Đăng nhập không thành công !');
-        }
-    }
->>>>>>> d12c48ef7219dc9621ba0e165d60dafed943455d
     public function getRegister(){
         return view('pages.register');
     }
@@ -66,7 +47,7 @@ class PagesController extends Controller
     //         'email.required'=>'bạn chưa nhập email',
     //         'email.min'=>'email phải lớn hơn 3 kí tự',
     //         'email.max'=>'email không quá 32 kí tự',
-            
+
     //         'name.required'=>'bạn chưa nhập tên',
     //         'name.min'=>'tên phải lơn hơn 3 kí tự',
     //         'name.max'=>'tên không quá 32 kí tự',
@@ -74,7 +55,7 @@ class PagesController extends Controller
     //         'password.required'=>'bạn chưa nhập mật khẩu',
     //         'password.min'=>'mật khẩu phải lớn hơn 3 kí tự',
     //         'password.max'=>'mật khẩu không quá 32 kí tự',
-            
+
     //         'dien_thoai.required'=>'bạn chưa nhập số điện thoại',
     //         'dien_thoai.min'=>'số điện thoại phải lớn hơn 1 kí tự',
     //         'dien_thoai.max'=>'số điện thoại không quá 11 kí tự',
@@ -104,15 +85,14 @@ class PagesController extends Controller
     $user->vip = $request->vip;
     $user->save();
     return view('pages/register')->with('thongbao', 'Đăng kí thành công !');
-    }   
+    }
 
     public function getDangxuat(){
         Auth::logout();
-<<<<<<< HEAD
+
         return redirect('pages.body');
-=======
         return redirect('pages/body');
->>>>>>> d12c48ef7219dc9621ba0e165d60dafed943455d
+
     }
 
 
