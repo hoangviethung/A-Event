fbe66a4dd498222e77717b7dc62146905c598972
hoangviethung <hoangviet.hung.009@gmail.com>
2019-12-12 09:51:55 +0700

    no message

--- ui-ux/src/js/main.js
+++ ui-ux/src/js/main.js
@@ -175,48 +175,78 @@ function getNameFile() {
 
 // ĐẾM SỐ NGƯỢC
 function countDownSale() {
-	$(".box-countdown").hide();
-
-	setTimeout(function() {
-		$(".box-countdown").fadeIn(2000);
-	}, 1000);
-
-	// Set the date we're counting down to
-	var temp = document.querySelector('.date-over p');
-
-	if (temp) {
-
-		var dateEND = new Date(temp.textContent).getTime();
-
-		// Update the count down every 1 second
-		var x = setInterval(function() {
+	$('.box-countdown').each(function() {
+		const timeEnd = $(this).attr('data-over');
+		var dateEND = new Date(timeEnd).getTime();
+		let _this = $(this);
+		let x = setInterval(function() {
 			// Get today's date and time
-			var now = new Date().getTime();
+			let now = new Date().getTime();
 
 			// Find the distance between now and the count down date
-			var distance = dateEND - now;
+			let distance = dateEND - now;
 
 			// Time calculations for days, hours, minutes and seconds
 
-			var days = Math.floor((distance / (1000 * 60 * 60 * 24)));
-			var hours = (days * 24) + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+			let days = Math.floor((distance / (1000 * 60 * 60 * 24)));
+			let hours = (days * 24) + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 			// Display the result in the element
 			try {
 				// document.getElementById("days").innerHTML = days;
-				document.getElementById("hours").innerHTML = hours;
-				document.getElementById("minutes").innerHTML = minutes;
-				document.getElementById("seconds").innerHTML = seconds;
+				_this.find(".hours").html(hours);
+				_this.find(".minutes").html(minutes);
+				_this.find(".seconds").html(seconds);
 				// If the count down is finished, write some text 
 				if (distance < 0) {
 					clearInterval(x);
-					document.getElementById("demo").innerHTML = "EXPIRED";
+					$(this).html("EXPIRED");
 				}
-			} catch (error) {}
+			} catch (error) {
+				console.log(error);
+				
+			}
 		}, 1000);
-	}
+	})
+
+	// Set the date we're counting down to
+	// var temp = document.querySelector('.date-over p');
+
+	// if (temp) {
+
+	// 	var dateEND = new Date(temp.textContent).getTime();
+
+	// 	// Update the count down every 1 second
+	// 	var x = setInterval(function() {
+	// 		// Get today's date and time
+	// 		var now = new Date().getTime();
+
+	// 		// Find the distance between now and the count down date
+	// 		var distance = dateEND - now;
+
+	// 		// Time calculations for days, hours, minutes and seconds
+
+	// 		var days = Math.floor((distance / (1000 * 60 * 60 * 24)));
+	// 		var hours = (days * 24) + Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	// 		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	// 		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	// 		// Display the result in the element
+	// 		try {
+	// 			// document.getElementById("days").innerHTML = days;
+	// 			document.getElementById("hours").innerHTML = hours;
+	// 			document.getElementById("minutes").innerHTML = minutes;
+	// 			document.getElementById("seconds").innerHTML = seconds;
+	// 			// If the count down is finished, write some text 
+	// 			if (distance < 0) {
+	// 				clearInterval(x);
+	// 				document.getElementById("demo").innerHTML = "EXPIRED";
+	// 			}
+	// 		} catch (error) {}
+	// 	}, 1000);
+	// }
 }
 
 // CLICK CHỌN FILE
